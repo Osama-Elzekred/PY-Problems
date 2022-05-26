7b704703ae47e52fd827095945aa84add5c5f39e
Osama Elzekred <osamaelzekred@gmail.com>
2022-05-26 03:24:55 +0200

    .

--- Hacker-Rank/DefultDict.py
+++ Hacker-Rank/DefultDict.py
@@ -1,14 +1,17 @@
-from collections import defaultdict 
+from collections import defaultdict
+
+
 def def_value():
     return "Not Present"
-      
+
+
 # Defining the dict
-d = defaultdict(lambda : 0)
+d = defaultdict(lambda: 0)
 # d["a"] = 1
 # d["b"] = 2
-  
-f=dict()
-d['a']=1
+
+f = dict()
+d['a'] = 1
 d['a'].append(4)
 d['b'].append(5)
 print(d)
@@ -17,36 +20,37 @@ d['python'].append("awesome")
 d['something-else'].append("not relevant")
 d['python'].append("language")
 for i in d.items():
-    print (i)
+    print(i)
 print(d['dsf'])
-print(len(d['dsf'])==0)
+print(len(d['dsf']) == 0)
 
 
 # Defining the dict
 d = defaultdict(int)
-   
+
 L = [1, 2, 3, 4, 2, 4, 1, 2]
-   
+
 # Iterate through the list
 # for keeping the count
 for i in L:
-       
+
     # The default value is 0
-    # so there is no need to 
+    # so there is no need to
     # enter the key first
     d[i] += 1
 
-
-
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from collections import defaultdict 
-
-n,m=input().split()
-b=[]
-d=defaultdict(list)
+n, m = input().split()
+b = []
+d = defaultdict(list)
 for i in range(int(n)):
     d[input()].append(i+1)
 for i in range(int(m)):
-    x=input()   
-    print(*d[x]) if len(d[x])>0 else print(-1)
+    x = input()
+    print(*d[x]) if len(d[x]) > 0 else print(-1)
+d = defaultdict()
+for i in range(8):
+    d[i] = i
+d.pop(5)
+d[5] = 32
+for key, value in d.items():
+    print(key, value)
--- Hacker-Rank/runner-up.py
+++ Hacker-Rank/runner-up.py
@@ -1,11 +1,16 @@
 
-def solve (arr):
-    set1=set(arr)
-    print("set : ",set1)
-    arr=list(set1)
-    print("arr : ",arr)
+def solve(arr):
+    set1 = set(arr)
+    print("set : ", set1)
+    arr = list(set1)
+    print("arr : ", arr)
     return arr[-2]
+
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
     print(solve(arr))
+
+    l = [1, 2, 1, 3, 4, 1]
+    print(l.index(1, 3))
